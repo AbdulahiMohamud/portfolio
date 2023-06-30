023bf4138fe2e2f153e03e9222c97823c3aec34d
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-06-29 21:59:33 -0700

    fixed the testing in the file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // eslint-disable-next-line no-undef
   ReactDOM.render(<App />, div);
 });
